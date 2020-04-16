12551801587169c9d8b98d7cb4903c81358bf7aa
Observer46 <microplayerpl@gmail.com>
2020-04-16 15:54:26 +0200

    Almost done cw05

--- cw05/zad1/main.c
+++ cw05/zad1/main.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+const int maxCommands = 256;
+const int maxArgsPerCommand = 256;
 
 void err_exit_limits(){
     fprintf(stderr,"Osiagnieto maksymalny limit ilosci komend lub argumentow do pojedynczej komendy!\n");
     exit(10);
 }
 
-int parse_command_line(FILE* command_file, char*** command_strings, int line_length){       // zwraca faktyczna ilosc komend, 
+int parse_command_line(FILE* command_file, char* command_strings[maxCommands][maxArgsPerCommand], int line_length){       // zwraca faktyczna ilosc komend, 
                                                                 // commandFile to wskaznik na aktualna pozycje w pliku
     char command_part[256] = "";  // Zakladam, ze pojedynczy kawalek komendy (nazwa badz tez argumenty) nie maja wiecej niz 256 znakow
 
@@ -19,46 +26,58 @@ int parse_command_line(FILE* command_file, char*** command_strings, int line_len
         if ( strcmp(command_part,"|") == 0 ){
             command_strings[command_count][arg_counter] = NULL;
             command_count++;
-            if (command_count >= 256)
+            if (command_count >= maxCommands)
                 err_exit_limits();
             arg_counter = 0;
+            continue;
+        }
+
+        if (command_strings[command_count][arg_counter] == NULL){
+            char* cmd_arg = (char*)calloc(256, sizeof(char));
+            strcpy(cmd_arg, command_part);
+            command_strings[command_count][arg_counter] = cmd_arg;
+        }
+        else{
+            strcpy(command_strings[command_count][arg_counter], command_part);
+        }
+
+        char next_char = getc(command_file);
+        //fseek(command_file, -1, SEEK_CUR);  // cofamy sie z powrotem
+
+        if ( next_char == '\n' || next_char == EOF){
+            newline_found = 1;
+            if (++arg_counter >= maxArgsPerCommand)
+                err_exit_limits();
+
+            command_strings[command_count][arg_counter] = NULL; // Ostanti NULL
+            command_count++;
         }
         
-        char cmd_arg[256];
-        strcpy(cmd_arg, command_part);
-
-        if ( cmd_arg[strlen(cmd_arg) - 1] == '\n' ){
-            newline_found = 1;
-            cmd_arg[strlen(cmd_arg) - 1] = '\0';
-        }
-        command_strings[command_count][arg_counter] = cmd_arg;
         arg_counter++;
 
-        if (arg_counter >= 256)
-            err_exit_limits();
+        if (arg_counter >= maxArgsPerCommand)
+            err_exit_limits();    
     }
 
-    command_strings[command_count][arg_counter] = NULL;         // Ostatni NULL
     return command_count;
 }
 
-int count_line_length(FILE* command_file){
-    int line_length = 0;
-    char chr = getc();
-    while (chr != EOF && chr !+ '\n'){
+int count_line_length(FILE* command_file, char* cmd){
+    int line_length = 1;
+    char chr = getc(command_file);
+    while (chr != EOF && chr != '\n'){
+        cmd[line_length-1] = chr;
         line_length++;
-        chr = getc();
+        chr = getc(command_file);
     }
+    cmd[line_length-1] = '\0';
     fseek(command_file, -line_length, SEEK_CUR);             // Wracamy na poczatek linii
     return line_length;
 }
 
 int main(int argc, char** argv){
-    const int maxCommands = 256;
-    const int maxArgsPerCommand = 256;
-
     if (argc != 2){
-        fprintf(stderr,"Prosze podac dokladnie 1 argument!\nArgument ten powinien byc sciezka do pliku z ktorego beda interpretowane polecenia\n");
+        fprintf(stderr,"Prosze podac dokladnie 1 - sciezke do pliku z komendami\n");
         exit(1);
     }
 
@@ -72,32 +91,69 @@ int main(int argc, char** argv){
 
     int pipe_array[maxCommands][2];
     char* command_strings[maxCommands][maxArgsPerCommand];
+    for (int i=0; i < maxCommands; i++)
+        for (int j=0; j < maxArgsPerCommand; j++)
+            command_strings[i][j] = NULL;       // Pomaga potem w zwalnaniu pamieci
+
+    char cmd[2048];     // Do wypisywania uzytej komendy
 
     while( !feof(command_file) ){        // Do sprawdzenia
-        int line_length = count_line_length(command_file);
+        int line_length = count_line_length(command_file, cmd);
+        printf("%s\n",cmd);
+        int command_count = parse_command_line(command_file, command_strings, line_length);
 
-        char* command_line = (char*)calloc(line_length, sizeof(char));
-        int command_count = parse_command_line(command_file);
+        for(int i=0; i < command_count; i++){
+            
+            if (i < command_count - 1)      // Otwieramy potok przed kazdym programem z wyjatkiem ostatniego
+                if (pipe(pipe_array[i]) == -1){
+                    fprintf(stderr,"Wywolanie pipe() sie nie powiodlo!\n");
+                    exit(3);
+                }
 
-        // if ( fgets(command_line, line_length, command_file) == NULL ){
-        //     free(command_line);
-        //     fclose(command_file);
-        //     fprintf(stderr,"Nie udalo sie odczytac linii komendy z pliku wejsciowego!\n");
-        //     exit(3);
-        // }
+            pid_t child_pid = fork();
+            if (child_pid == -1){
+                fprintf(stderr,"Wywolanie fork() sie nie powiodlo!\n");
+                exit(4);
+            }
+
+            if (child_pid == 0){
+                
+                if (i-1 >= 0){
+                    dup2(pipe_array[i-1][0], STDIN_FILENO);
+                    close(pipe_array[i-1][1]);      // Nie bedzie tutaj nic pisac
+                }
 
 
-        for (int i=0; i < command_count; i++){  // Proces macierzysty nie uzywa potokow, wiec zamyka obie koncowki
-            fclose(pipe_array[i][0]);
-            fclose(pipe_array[i][1]);
+                if (i < command_count - 1){
+                    dup2(pipe_array[i][1], STDOUT_FILENO);
+                    close(pipe_array[i][0]);        // Nie bedzie stad nic czytac 
+                }
+
+                execvp(command_strings[i][0],command_strings[i]);
+                fprintf(stderr,"Wywolanie execvp(%s) sie nie powiodlo!\n", command_strings[i][0]);
+                exit(5);
+                
+            }
+            else{
+                if (i-1 >= 0){               // Zamykamy tylko te, ktore sa nieuzywane (zamykanie wczesniej spowoduje, ze potomek dostanie zamkniety potok)
+                    close(pipe_array[i-1][0]);      // Proces macierzysty nie uzywa potokow, wiec zamyka obie koncowki
+                    close(pipe_array[i-1][1]);      // Ale zamykamy tylko te, ktore sa juz nie uzywane    
+                }
+
+            }
         }
+
         
-        for (int i=0; i < command_count; i++)   ,// Proces macierzysty czeka na wszystkich potomkow
+        for (int i=0; i < command_count; i++)   // Proces macierzysty czeka na wszystkich potomkow
             wait(NULL);
-        free(command_line)
+        
+        printf("\n");
     }
 
-    
+    for (int i=0; i < maxCommands; i++)
+        for (int j=0; j < maxArgsPerCommand; j++)
+            if (command_strings[i][j] != NULL)
+                free(command_strings[i][j]);    
 
     fclose(command_file);
 
